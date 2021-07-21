313ef549b76e8fd8ea7d0ce26e52af486add2904
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-07-21 13:04:09 +0200

    Change filename for the filterer.

--- filter_vocab.py
+++ filter_vocab.py
@@ -191,7 +191,7 @@ def remove(arr, idx):
 if __name__ == "__main__":
   filename = 'data/vocab/repo_vocab.json'
   filename_test = 'data/vocab/test/test_vocab.json'
-  filterer = VocabFilterer(filename_test)
+  filterer = VocabFilterer(filename)
   try:
     filterer.filter()
   except Exception as exc:
