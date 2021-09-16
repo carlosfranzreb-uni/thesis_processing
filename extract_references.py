0c720ed3b96087949d3ffba65f01c564eae6e32a
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-09-16 17:47:57 +0200

    WIP to extract references from the PDFs.

---
+++ extract_references.py
@@ -0,0 +1,70 @@
+""" The PDFs can be found in the DIDL metadata format for depositonce and edoc
+and XOAI for refubium. The URL of the PDF can then be passed to 'refextract',
+which extracts the references.  """
+
+
+import xml.etree.ElementTree as ET
+import os
+import logging
+from time import sleep, time
+from pathlib import Path
+
+
+import requests
+# from refextract import extract_references_from_url
+from tika import parser
+
+
+oai = '{http://www.openarchives.org/OAI/2.0/}'
+didl = '{urn:mpeg:mpeg21:2002:02-DIDL-NS}'
+
+
+def get_didl_pdf(base_url, id):
+  """ Download the PDF found in the metadata in DIDL format. """
+  filename = id.split('/')[-1]
+  req = f'{base_url}?verb=GetRecord&identifier={id}&metadataPrefix=didl'
+  record = ET.fromstring(requests.get(req).text)
+  pdf_url = record.find(f'{oai}GetRecord').find(f'{oai}record') \
+    .find(f'{oai}metadata').find(f'{didl}DIDL').find(f'{didl}Item') \
+    .find(f'{didl}Component').find(f'{didl}Resource').attrib['ref']
+  pdf_res = requests.get(pdf_url)
+  f = Path(f'data/pdf/{filename}.pdf')
+  f.write_bytes(pdf_res.content)
+  logging.info(f'PDF file of {filename} downloaded.')
+  return filename
+
+
+def parse_pdf(filename):
+  """ Extract the text of the PDF and store it in a TXT file. Delete the PDF
+  file afterwards. """
+  try:
+    pdf_file = f'data/pdf/{filename}.pdf'
+    pdf = parser.from_file(pdf_file)
+    if pdf["content"] is not None:
+      with open(f'data/txt/{filename}.txt', 'w', encoding='utf8') as f:
+        f.write(pdf["content"])
+      os.remove(pdf_file)
+      logging.info(f'TXT file of {filename} created.')
+  except requests.exceptions.ReadTimeout:
+    logging.error(f"Parsing of {filename} failed.")
+
+
+# def get_references(url):
+#   """ Return the references of the PDF document located at the given URL. """
+#   return extract_references_from_url(url)
+
+
+if __name__ == '__main__':
+  # url = 'https://depositonce.tu-berlin.de/bitstream/11303/6967/1/c5cp00289c.pdf'
+  logging.basicConfig(
+    filename=f"logs/extractrefs_{str(int(time()))}.log",
+    format='%(asctime)s %(message)s',
+    level=logging.INFO
+  )
+  base_url = 'https://depositonce.tu-berlin.de/oai/request'
+  id = 'oai:depositonce.tu-berlin.de:11303/6967'
+  filename = get_didl_pdf(base_url, id)
+  parse_pdf(filename)
+  # refs = get_references(url)
+  # print(refs)
+  # TODO: txt files works, now get refs
