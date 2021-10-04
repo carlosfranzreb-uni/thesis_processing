2c41a83562feaa1d2d326c9b37cfa167fdb97a71
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-10-04 11:30:13 +0200

    Correct several bugs.

--- ert/referee_ert.py
+++ ert/referee_ert.py
@@ -1,7 +1,7 @@
-""" Functions related to the creation of the ERT of a venue. get_publications()
-creates a file that maps publications (list of values) to venues (keys).
-create_ert(venue, sample_size) concatenates the SRTs of a sample of publications
-from the given venue. """
+""" Functions related to the creation of the ERT of a referee. get_theses()
+creates a file that maps theses (list of values) to referees (keys).
+create_ert(these, sample_size) concatenates the SRTs of a sample of theses
+from the given referee. """
 
 
 import json
@@ -9,36 +9,40 @@ from random import sample
 
 
 def get_referees():
-  """ For each venue found in 'json/dim/all/relevant_venues.json', gather all
-  the referees that belong to it and dump them in a file. """
-  venues = json.load(open('../data/json/dim/all/relevant_venues.json'))
+  """ Retrieve the referees of the files in the 'data/processed' folder and
+  dump them in a file with them as keys and a list of their theses as values."""
   referees = {}
-  for publication_id, venue in venues.items():
-    if venue not in referees:
-      referees[venue] = []
-    referees[venue].append(publication_id)
+  for repo in ('depositonce', 'edoc', 'refubium'):
+    data = json.load(open(f'../data/processed/dim/{repo}.json'))
+    for doc in data:
+      if doc['type'][1] == 'thesis':
+        for contributor in doc['authors']:
+          if contributor[1] == 'referee':
+            if contributor[0] not in referees:
+              referees[contributor[0]] = []
+            referees[contributor[0]].append(doc['id'])
   json.dump(referees, open('../data/json/dim/all/ert/referees.json', 'w'))
 
 
 def get_erts():
-  """ For each venue in 'venue_publications' (the file created by
-  get_publications()), call create_ert(). Dump the results in a file. """
-  publications = json.load(open('../data/json/dim/all/ert/referees.json'))
+  """ For each referee in 'referees' (the file created by get_referees()), call
+  create_ert(). Dump the results in a file. """
+  referees = json.load(open('../data/json/dim/all/ert/referees.json'))
   data = json.load(open('../data/json/dim/all/relevant_data.json'))
-  erts = {referee: create_ert(ids, data) for referee, ids in publications.items()}
+  erts = {referee: create_ert(ids, data) for referee, ids in referees.items()}
   json.dump(erts, open('../data/json/dim/all/ert/referee_erts.json', 'w'))
 
 
-def create_ert(publications, data, sample_size=4):
-  """ Given the publications of a venue and a sample size, concatenate the SRTs
-  (i.e. title and abstract) of 'sample_size' random publications of the venue.
-  Separate the texts of the publications with three slashes '///', so no ngrams
+def create_ert(theses, data, sample_size=4):
+  """ Given the theses of a venue and a sample size, concatenate the SRTs
+  (i.e. title and abstract) of 'sample_size' random theses of the venue.
+  Separate the texts of the theses with three slashes '///', so no ngrams
   are formed using tokens of different texts. Return the concantenated texts as
-  well as a list with the IDs of the used publications. """
-  if len(publications) <= sample_size:
-    ids = publications
+  well as a list with the IDs of the used theses. """
+  if len(theses) <= sample_size:
+    ids = theses
   else:
-    ids = sample(publications, sample_size)
+    ids = sample(theses, sample_size)
   ert = ''
   for id in ids:
     for text in data[id].values():
