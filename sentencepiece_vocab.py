e672d4e54892e9d15d03395f76d0430454d5f953
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2021-11-23 17:12:15 +0100

    Create input for sentencepiece.

---
+++ sentencepiece_vocab.py
@@ -0,0 +1,32 @@
+""" Compute the SentencePiece vocabulary of our corpus to compare it to the
+one of SciBERT. SciVOCAB has 30k terms, same as BERT. """
+
+
+import json
+import sentencepiece as spm
+from nltk import sent_tokenize
+
+
+def create_input():
+  """ Input of the trainer must be a TXT file with one sentence of raw text
+  in each line. """
+  with open('data/txt/raw_text.txt', 'w', encoding='utf-8') as f:
+    for line in load_data():
+      f.write(line + '\n')
+
+
+def load_data():
+  """ Lazy load the sentences of all titles and abstracts. """
+  all_data = json.load(open(
+    'data/json/dim/all/relevant_data.json',
+    encoding='utf-8'
+  ))
+  for data in all_data.values():
+    for text in data.values():
+      if text is not None:
+        for sentence in sent_tokenize(text):
+          yield sentence
+
+
+if __name__ == "__main__":
+  create_input()
