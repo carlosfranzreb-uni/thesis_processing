29b93fc4c5b841a30831eb0ce85f9dab26029760
Carlos Franzreb <carlos.franzreb@fokus.fraunhofer.de>
2022-01-10 15:19:23 +0100

    Given a vocab, remove all entries that comprise more than one word.

--- create_vocab.py
+++ create_vocab.py
@@ -86,21 +86,35 @@ def filter(phrases):
   return filtered
 
 
+def remove_ngrams(vocab_file, dump_file):
+  """ Given a vocab, remove all entries that comprise more than one word. """
+  vocab = json.load(open(vocab_file))
+  new_vocab = {}
+  for entry in vocab:
+    if ' ' not in entry:
+      new_vocab[entry] = vocab[entry]
+  json.dump(new_vocab, open(dump_file, 'w'))
+
+
 if __name__ == "__main__":
-  start = int(time())
-  logging.basicConfig(
-    filename=f"logs/vocab_{start}.log",
-    format='%(asctime)s %(message)s',
-    level=logging.INFO
+  # start = int(time())
+  # logging.basicConfig(
+  #   filename=f"logs/vocab_{start}.log",
+  #   format='%(asctime)s %(message)s',
+  #   level=logging.INFO
+  # )
+  # data = json.load(open('data/json/dim/all/data.json'))
+  # tokenizer = SpacyTokenizer('en_core_web_sm')
+  # lemmatizer = WordNetLemmatizer()
+  # tagger = SequenceTagger.load('upos-fast')
+  # logging.info('About to create a vocabulary out of the repositories')
+  # logging.info('Using the spacy tokenizer with the "en_core_web_sm" model.')
+  # logging.info('Using the WordNetLemmatizer of NLTK.')
+  # logging.info('Using the upos-fast model of flair for POS-tagging.')
+  # logging.info('Extracting N-grams of up to length 4.')
+  # vocab = create_vocab(data, tokenizer, tagger, lemmatizer, process)
+  # json.dump(vocab, open(f'data/vocab/repo_vocab_{start}.json', 'w'))
+  remove_ngrams(
+    'data/vocab/repo_vocab_step_1.json',
+    'data/vocab/repo_vocab_1grams.json'
   )
-  data = json.load(open('data/json/dim/all/data.json'))
-  tokenizer = SpacyTokenizer('en_core_web_sm')
-  lemmatizer = WordNetLemmatizer()
-  tagger = SequenceTagger.load('upos-fast')
-  logging.info('About to create a vocabulary out of the repositories')
-  logging.info('Using the spacy tokenizer with the "en_core_web_sm" model.')
-  logging.info('Using the WordNetLemmatizer of NLTK.')
-  logging.info('Using the upos-fast model of flair for POS-tagging.')
-  logging.info('Extracting N-grams of up to length 4.')
-  vocab = create_vocab(data, tokenizer, tagger, lemmatizer, process)
-  json.dump(vocab, open(f'data/vocab/repo_vocab_{start}.json', 'w'))
